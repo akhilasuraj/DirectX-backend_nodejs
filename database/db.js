10449591c1c0a5050852d07d337c5237810e11ac
Akhila Suraj <akhila.abesinghe@gmail.com>
2019-03-29 11:50:00 +0530

    DB URL ADDED

--- database/db.js
+++ database/db.js
@@ -1,19 +1,22 @@
  const Sequelize = require("sequelize")
  const db={}
- const sequelize = new Sequelize("login","root","",{
-    host : "localhost",
-    dialect : "mysql",
-    operatorAliases: false,
+ const sequelize = new Sequelize('mysql://bd9b5c7dace288:6c4d9d48@us-cdbr-iron-east-03.cleardb.net/heroku_4c73b39b13258ec?reconnect=true')
 
-     pool:{
-          max:5,
-          min:0,
-          acquire:30000,
-          idle:10000
 
-     }
+//  const sequelize = new Sequelize("login","root","",{
+//     host : "localhost",
+//     dialect : "mysql",
+//     operatorAliases: false,
 
- })
+//      pool:{
+//           max:5,
+//           min:0,
+//           acquire:30000,
+//           idle:10000
+
+//      }
+
+//  })
     db.sequelize=sequelize
     db.Sequelize=Sequelize 
 
